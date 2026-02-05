5fbec8a0c645b25f8995e4a48ce0d8c955fc7daf
SARO-YOU <saroyouyou67@gmail.com>
2026-02-06 00:47:06 +0300

    Fix app initialization order

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
-from models import Product
-from routes.products import products_bp
-app.register_blueprint(products_bp)
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+from models import Product
+from routes.products import products_bp
+
+app.register_blueprint(products_bp)
 
 @app.route("/")
 def home():
